93374c38eb4126c86ef285f00f93166ef9d51564
eduardalbiol <eduard.romero@alliantum.com>
2021-09-30 12:51:28 +0200

    Section video 32 Done

--- src/components/Cart/styles.js
+++ src/components/Cart/styles.js
@@ -1,11 +1,25 @@
 import styled from "styled-components";
+import { StyledLink } from "../StyledLink";
 
-export const CartWrapper = styled.div`
+export const CartWrapper = styled(StyledLink).attrs(() => ({
+    to: "/cart"
+}))`
 margin-left: auto;
 display: flex;
+color: black;
+text-decoration: none;
 
 >div:last-child{
     padding-left: 8px;
     margin: auto 0;
 }
+&:hover{
+   text-decoration: underline; 
+}
 `
+
+/*
+StyledLink explanation on video section 32.
+Link provided from professor:
+https://styled-components.com/docs/advanced#styling-normal-react-components
+*/
