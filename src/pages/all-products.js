3e8fe02b9066cd1ae2d169bf9bd15104ffc688c3
eduardalbiol <eduard.romero@alliantum.com>
2021-10-06 10:09:35 +0200

    Video 49 comments added

--- src/pages/all-products.js
+++ src/pages/all-products.js
@@ -24,9 +24,27 @@ export default function AllProducts() {
 
     const collectionProductMap = {}
 
-    const filterByCategory = (product) => {
+
+    if (collections) { //'if' just to check if ciollections have value.
+
+        //1.
+        //For each collection we want to assign an id that will be stored to 'collectionProductMap'.
+        collections.forEach(collection => {
+            collectionProductMap[collection.shopifyId] = {}; // {collectionId1, collectionId2, ...} / With '={}' we are saying that the 'collectionId' will have one ore more values inside. In this case the productId 
+
+            //Loop through products of each collection:
+            collection.products.forEach(product => {
+                collectionProductMap[collection.shopifyId][product.shopifyId] = true //{collectionId1: productId1=true, productId2=true, ...} / The [] after another [] means that the second [] is the data inside the object. In this case collection will have inside the products.
+            })
+        })
 
     }
+
+    console.log("---> collectionProductMap: ", collectionProductMap);
+
+    const filterByCategory = (product) => {
+
+    } //It's within this function where we want to return whether the category matches our category or not.
 
     const filteredProducts = products.filter(filterByCategory)
 
@@ -36,7 +54,7 @@ export default function AllProducts() {
             <Content> {/* styled component created on top of this page component. */}
                 <Filters />
                 <div>
-                    <ProductsGrid products={products} /> {/* We reuse the ProductsGrid component and we pass/send the products prop. */}
+                    <ProductsGrid products={products} /> {/* We reuse the ProductsGrid component and we pass/send the products prop to the child component. */}
                 </div>
             </Content>
 
@@ -46,8 +64,74 @@ export default function AllProducts() {
 
 
 /*
-The 12 products are displayed. We want now to filter out all the ones that don't match
-the any of the pressed/checked categories.
+We want to filter out products if they don't match any of the categories that we have
+selected to filter.
 
-A)
+
+1. The content of 'collectionProductMap':
+
+///////////////////////////////////////////////////////////////////
+///-***** C R E A T I N G   D Y N A M I C   O B J E C T S *****-///
+///////////////////////////////////////////////////////////////////
+
+It contains an object with objects inside. The objects inside are the shopifyId's of
+each collection.
+
+Each collection shopifyId has inside a list of the id's of its products and an equal to true.
+
+E.G:
+
+{
+    'collection1Id': {'product1Id': true, 'product2Id': true, 'product3Id': true},
+    'collection2Id': {'product1Id': true, 'product2Id': true, 'product3Id': true},
+    'collection3Id': {'product1Id': true, 'product2Id': true, 'product3Id': true},
+    'collection4Id': {'product1Id': true, 'product2Id': true, 'product3Id': true}
+}
+
+- WHY WE WANT THIS STRUCTURE? The reason we want it to look like this is that we can easily check if a particular
+  product belongs to a specific collection by going:
+
+
+
+- KEY CONCEPT: Explanation of the 2 [][]:
+    The [] after another [] means that the second [] is the data inside the object.
+    In this case collection will have inside the products. As we are in a forEach this means that
+    ---> E.G: collectionMap[collectionId][productId]
+
+
+- KEY CONCEPT: Explanation of the '={}':
+    - In order to be able store data inside of an object we need to create that room for it.
+    - E.G: collectionProductMap[collection.shopifyId] = {};
+    - In this case we will want to store products inside each collection (the [][]). If we don't
+      give it an space before there will be no room to allocate the products.
+    - What we are doing is giving it space to allocate products in the next forEach.
+    - We are doing this: 'collection2Id': {},
+    - The same happened with 'const collectionProductMap = {}'. We created a space for the collections id
+      to be inside.
+
+
+
+- Flow in the forEach loop:
+     - collections.forEach() loop: It stores inside collectionProductMap 'collection1Id'
+     ---> goes down to the second forEach inside the first forEach.
+     - collection.products.forEach() loop: We are in collection1 and we add inside of it
+     its products.
+
+     {
+       Step 1. 'collection1Id'                                                                    <--- In first forEach collection1Id is stored.
+       Step 2. 'collection1Id': {'product1Id': true}                                              <--- In second forEach (inside first forEach) product1Id is stored.
+       Step 3. 'collection1Id': {'product1Id': true, 'product2Id': true}                          <--- In second forEach (inside first forEach) product2Id is stored.
+       Step 4. 'collection1Id': {'product1Id': true, 'product2Id': true, 'product3Id': true}      <--- In second forEach (inside first forEach) product3Id is stored.
+     }
+
+     . . .
+
+     At the end we have all collectionsId with its productsId inside:
+
+     {
+    'collection1Id': {'product1Id': true, 'product2Id': true, 'product3Id': true},
+    'collection2Id': {'product1Id': true, 'product2Id': true, 'product3Id': true},
+    'collection3Id': {'product1Id': true, 'product2Id': true, 'product3Id': true},
+    'collection4Id': {'product1Id': true, 'product2Id': true, 'product3Id': true}
+     }
 */
