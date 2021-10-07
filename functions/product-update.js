238aa898934bc64118bec32c3f00e522deeb5cd1
eduardalbiol <eduard.romero@alliantum.com>
2021-10-07 15:20:42 +0200

    Video Section 61 3/4 Done

--- functions/product-update.js
+++ functions/product-update.js
@@ -1,5 +1,6 @@
 const faunadb = require("faunadb");
 const verifyWebhookIntegrity = require("shopify-verify-webhook")
+const axios = require("axios");
 
 const q = faunadb.query;
 const client = new faunadb.Client({
@@ -11,10 +12,41 @@ exports.handler = function (event, context, callback) {
         event.headers["x-shopify-hmac-sha256"], event.body);
 
     if (isValid) {
-        callback(null, {
-            statusCode: 200,
-            body: "Hello World!"
+        const body = JSON.parse(event.body);
+        const { id } = body;
+        delete body.updated_at;
+        body.variants.forEach(variant => {
+            delete variant.updated_at;
+            delete variant.inventory_quantity;
+            delete variant.old_inventory_quantity;
         });
+
+        const bodyString = JSON.stringify(body);
+
+        client.query(q.Get(q.Match(q.Index(("product_by_id"), id)))).then(result => {
+            if (result.data.product !== bodyString) {
+                client.query(q.Update(result.ref, {
+                    data: { product: bodyString },
+                })
+                ).then(() => {
+                    // call rebuild
+                    axios.post(process.env.NETLIFY_BUILD_URL)
+                }).catch(e => {
+                    console.log("error updating product: ", e);
+                });
+            }
+        }).catch(() => {
+            client.query(q.Create(q.Collection("products"), {
+                data: { id, product: bodyString },
+            })
+            ).then(() => {
+                // cal rebuild
+                axios.post(process.env.NETLIFY_BUILD_URL)
+            }).catch((e) => {
+                console.log("erroradding to db:", e);
+            });
+        });
+
     } else {
         callback(null, {
             statusCode: 403,
