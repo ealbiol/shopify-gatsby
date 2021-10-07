2ae23d2233c3a82abca66f1d197a4b875aa48d1a
eduardalbiol <eduard.romero@alliantum.com>
2021-10-07 17:28:41 +0200

    test6

--- functions/product-update.js
+++ functions/product-update.js
@@ -12,10 +12,30 @@ exports.handler = function (event, context, callback) {
         event.headers["x-shopify-hmac-sha256"], event.body);
 
     if (isValid) {
-        callback(null, {
-            statusCode: 200,
-            body: "Hello World!",
+        const body = JSON.parse(event.body);
+        const { id } = body;
+        delete body.updated_at;
+        body.variants.forEach(variant => {
+            delete variant.updated_at;
+            delete variant.inventory_quantity;
+            delete variant.old_inventory_quantity;
         });
+
+        const bodyString = JSON.stringify(body);
+
+        client.query(q.Get(q.Match(q.Index("product_by_id"), id))).then(result => {
+            if (result.data.product !== bodyString) {
+                //call rebuild
+            }
+        }).catch(() => {
+            client.query(q.Create(q.Collection("products"), {
+                data: { id, product: bodyString },
+            })
+            ).then(() => {
+                // call rebuild. CONTINUE HERE: 14:42
+            })
+        });
+
     } else {
         callback(null, {
             statusCode: 403,
